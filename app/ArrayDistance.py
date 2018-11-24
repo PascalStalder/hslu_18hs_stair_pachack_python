73edec8fc7a7863364f9b5c7e30a68dc9980bd52
Larissa <l.schuler.94@gmail.com>
2018-11-24 14:31:20 +0100

    capsule availability

--- app/ArrayDistance.py
+++ app/ArrayDistance.py
@@ -1,15 +1,18 @@
 from dto.HelperDTOs import PublicFields
 import numpy as np
-from scipy.spatial.distance import cdist
+
 
 class ArrayDistance():
+    def __init__(self, map, side):
+        self.map = map
+        self.side = side
 
-    @staticmethod
-    def closest_capsule(map, myPosition, side):
-        if side == 0:
-            field = map[:][:int(len(map[:][0])/2)]
+
+    def getCapsuleList(self):
+        if self.side == 0:
+            field = self.map[:][:int(len(map[:][0]) / 2)]
         else:
-            field = map[:][-17:]
+            field = self.map[:][-17:]
         capsule_list = []
         field = np.array(field)
         for i in range(0, field.shape[0]):
@@ -17,10 +20,26 @@ class ArrayDistance():
             for j, obj in enumerate(row):
                 if obj == PublicFields.CAPSULE:
                     capsule_list.append([i, j])
-        print(myPosition)
-        print(capsule_list)
-        distances = [(myPosition[0]-capsule[0])**2 + (myPosition[1]-capsule[1])**2 for capsule in capsule_list]
-        result = capsule_list[np.argmin(distances)]
-        print('closest capsule: ', result)
-        capsulePosition = result
+        return capsule_list
+
+
+    def closest_capsule(self, myPosition):
+        capsulePosition = None
+        capsule_list = self.getCapsuleList()
+        if self.capsule_available():
+            distances = [(myPosition[0] - capsule[0]) ** 2 + (myPosition[1] - capsule[1]) ** 2 for capsule in
+                         capsule_list]
+            result = capsule_list[np.argmin(distances)]
+            print('closest capsule: ', result)
+            capsulePosition = result
+            print(myPosition)
+            print(capsule_list)
+
         return capsulePosition
+
+
+    def capsule_available(self):
+        if len(self.getCapsuleList()) == 0:
+            return False
+        else:
+            return True
